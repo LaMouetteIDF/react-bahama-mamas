75f5361bd18aa9212788eb9cf6bbbf58d7ea8e3c
boysers <boyser2015@gmail.com>
2022-05-25 05:48:19 +0200

    first commit

---
+++ src/components/index.js
@@ -0,0 +1,9 @@
+// import React from 'react';
+// import cn from 'classnames';
+
+// import styles from './UntitledPage.module.scss';
+
+
+import UntitledPage from './UntitledPage';
+
+export default { UntitledPage };
